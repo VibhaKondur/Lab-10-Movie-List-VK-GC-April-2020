631fdc45d2fadfa8b235e95d917fe5afcdb50434
VibhaKondur <62919352+VibhaKondur@users.noreply.github.com>
2020-04-30 22:02:17 -0400

    Lab 10 Movie List

---
+++ Lab 10 VK GC April 2020/MovieList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_10_VK_GC_April_2020
+{
+
+    class MovieList
+    {
+        //private fields - movie title, movie category
+        //syntax - these start with a lower-case letter - Camel Case
+        private string movieTitle;
+        private string movieCategory;
+
+
+        //public properties - getters & setters
+        //because these are public, these are what we use to access the properties of an object
+        //syntax - these start with an upper-case letter - Pascal Case
+        public string MovieTitle
+        {
+            get //referenced when we call this property from an object 
+            {
+                return movieTitle;
+            }
+            set //used when we give the property its value
+            {
+                movieTitle = value;
+            }
+
+        }
+        public string MovieCategory
+        {
+            get
+            {
+                return movieCategory;
+            }
+            set
+            {
+                movieCategory = value;
+            }
+        }
+
+
+        //constructor(s)
+        //all classes come with a defualt constructor. BUT PLEASE NOTE that the minute a default constructor is overloaded, it no longer exists
+        //This overloaded constructor will allow me to give an object's properties value when I create the object. 
+        public MovieList(string _movieTitle, string _movieCategory)
+        {
+            movieTitle = _movieTitle; //setting our field equal to the value coming from where we create out object
+            movieCategory = _movieCategory;
+
+        }
+
+        public MovieList()
+        {
+            //initializing variable in default constructor
+            movieTitle = "Inside Out";
+            movieCategory = "animated";
+        }
+
+        //methods
+
+        public void PrintClassmateInfo()
+        {
+            Console.WriteLine($"Since you want to watch a {movieCategory} movie, you can pick from these {movieTitle}. \n");
+        }
+
+    }
+}
---
+++ Lab 10 VK GC April 2020/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Lab_10_VK_GC_April_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MovieList Tony = new MovieList();
+
+            Console.WriteLine(Tony.GetType());
+
+            //will print out the DEFAULT values of all of the properties of Tony
+            //the DEFAULT values you see here are being set manually in the default constructor in
+            //the Classmate class
+            //If we didn't do that, the compiler would display the default C-SHARP values
+            Console.WriteLine($"{Tony.Name} is from {Tony.HomeTown}. " +
+                $"He was born on {Tony.Birthday}. " +
+                $"His favorite number is {Tony.FavoriteNumber}. \n");
+
+            //
+            Tony.Name = "Tony Prosciutto";
+            Tony.HomeTown = "Jersey";
+            Tony.Birthday = DateTime.Parse("1/1/2001");
+            Tony.FavoriteNumber = 1;
+
+            //will print out Tony without the default values
+            Console.WriteLine($"{Tony.Name} is from {Tony.HomeTown}. " +
+            $"He was born on {Tony.Birthday.ToShortDateString()}. " +
+            $"His favorite number is {Tony.FavoriteNumber}.");
+
+            List<Classmate> classmates = new List<Classmate>();
+            FillListOfClassmates(classmates);
+
+            foreach (Classmate friend in classmates)
+            {
+                //print out the classmates
+                friend.PrintClassmateInfo();
+            }
+        }
+    }
+}
